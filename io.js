8c090583f298e6eba45569cc454b9a8d850e630a
agung <guungofficial@gmail.com>
2019-10-07 16:42:30 +0700

    initial commit

---
+++ io.js
@@ -0,0 +1,1 @@
+$("questions").append("<input maxlength="1" data-row="1" data-column="1" data-crossed="0" data-letter="" data-dir="" data-across-key="0" data-down-key="0" id=\"row_1_col_1\" type=\"text\" value="" style=\"text-transform: uppercase;display:none;\" />");
